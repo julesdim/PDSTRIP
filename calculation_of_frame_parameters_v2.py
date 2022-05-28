991ae39602a2732ebb0a5ba0a854c57cfad7557b
jules <jules.dimauro@gmail.com>
2022-05-28 15:25:12 +0200

    nouvelle version

--- calculation_of_frame_parameters_v2.py
+++ calculation_of_frame_parameters_v2.py
@@ -31,31 +31,32 @@ def collection_of_coordinates(filename):
     file = open(filename, "r", encoding="utf-8")
     the_lines = csv.reader(file)
     line_counter = 0
-    line_of_the_frame_beginning = 0
-    line_of_the_frame_ending = 1000
+    start_line_of_the_frame = 0
+    end_line_of_the_frame = 1000
     boolean_for_beginning_of_the_frame = True  # we can know if we are at the beginning of the frame to get the x coordinate of the frame
     form_of_the_ship = shape.Form()  # initialisation of the list of coordinates
     for line in the_lines:
-        new = line[0].strip().split()  # formating the line
+        line_formatted = line[0].strip().split()  # formating the line
         if line_counter == 0:
-            nb_f_tot = float(new[0])  # to know the number of frame
-        if line_counter != 0 and len(new) == 1 and boolean_for_beginning_of_the_frame:
-            x = (float(new[0]))  # if there is just one coordinate, it is the position along x axis, if beg_frame==True
-            line_of_the_frame_beginning = line_counter
-            frame_act = fr.Frames(x)
+            total_number_of_frame = float(line_formatted[0])  # to know the number of frame
+        if line_counter != 0 and len(line_formatted) == 1 and boolean_for_beginning_of_the_frame:
+            x_coordinate = (float(line_formatted[
+                                      0]))  # if there is just one coordinate, it is the position along x axis, if beg_frame==True
+            start_line_of_the_frame = line_counter
+            current_frame = fr.Frames(x_coordinate)
             boolean_for_beginning_of_the_frame = False  # As we just passed the beg of the frame, next is false
-        if line_counter != 0 and len(new) == 3:
-            y = (float(new[0]))
-            z = (-float(new[1]))
-            frame_act.__append__((y, z))  # we append to the list
-            if y != 0:
-                frame_act.__append__((-y, z))
+        if line_counter != 0 and len(line_formatted) == 3:
+            y_coordinate = (float(line_formatted[0]))
+            z_coordinate = (-float(line_formatted[1]))
+            current_frame.__append__((y_coordinate, z_coordinate))  # we append to the list
+            if y_coordinate != 0:
+                current_frame.__append__((-y_coordinate, z_coordinate))
             boolean_for_beginning_of_the_frame = True  # the next time len(new)==1 it will be the beginning of a new frame
-        if len(new) == 1 and line_counter == line_of_the_frame_beginning + 1:
-            nb_points = int(new[0])
-            line_of_the_frame_ending = line_counter + nb_points
-        if line_counter == line_of_the_frame_ending:
-            form_of_the_ship.__append__(frame_act)
+        if len(line_formatted) == 1 and line_counter == start_line_of_the_frame + 1:
+            number_points_of_the_current_frame = int(line_formatted[0])
+            end_line_of_the_frame = line_counter + number_points_of_the_current_frame
+        if line_counter == end_line_of_the_frame:
+            form_of_the_ship.__append__(current_frame)
         line_counter += 1
     file.close()
     return form_of_the_ship
@@ -75,26 +76,29 @@ def collection_of_mass(filename):
         it is a list of mass, defined by the information mentioned above but with mass per
         meter computed at the beginning and at the end
      """
-    fichier = open(filename, "rt")
-    list_of_masses = ld.Loading()  # initialisation of the list of masses
-    the_lines = fichier.readlines()
+    file = open(filename, "rt")
+    weight_loading = ld.Loading()  # initialisation of the list of masses
+    the_lines = file.readlines()
     total_mass = 0  # to check the total mass
     for line in the_lines:
-        line = line.strip("\n").split(";")  # we stop the line to the \n and we cut the information where there is a ";"
-        m = float(line[0])  # the first info is the object mass
-        total_mass += m
-        xb = float(line[1])  # the second is the beginning
-        xe = float(line[2])  # the end
-        xg = float(line[3])  # the exact center of gravity
-        yr = float(line[4])  # the turning radius
-        z = float(line[5])  # the position along z axis of the center of gravity
-        mb_per_meter, me_per_meter = m / (xe - xb), m / (xe - xb)
-        if m != 0:
-            masse = mass.Mass(m, xb, xe, xg, yr, z, mb_per_meter, me_per_meter)
-            if xg != (xb + xe) / 2:
-                masse.calcul_xg_not_the_mid()
-            list_of_masses.__append__(masse)
-    return list_of_masses
+        line_formatted = line.strip("\n").split(
+            ";")  # we stop the line to the \n and we cut the information where there is a ";"
+        current_weight = float(line_formatted[0])  # the first info is the object mass
+        total_mass += current_weight
+        x_beginning = float(line_formatted[1])  # the second is the beginning
+        x_end = float(line_formatted[2])  # the end
+        x_coordinate_CoG = float(line_formatted[3])  # the exact center of gravity
+        y_coordinate_CoG = float(line_formatted[4])  # the turning radius
+        z_coordinate_CoG = float(line_formatted[5])  # the position along z axis of the center of gravity
+        linear_density_x_beginning, linear_density_x_end = current_weight / (x_end - x_beginning), current_weight / (
+                x_end - x_beginning)
+        if current_weight != 0:
+            current_mass = mass.Mass(current_weight, x_beginning, x_end, x_coordinate_CoG, y_coordinate_CoG,
+                                     z_coordinate_CoG, linear_density_x_beginning, linear_density_x_end)
+            if x_coordinate_CoG != (x_beginning + x_end) / 2:
+                current_mass.calcul_xg_not_the_mid()
+            weight_loading.__append__(current_mass)
+    return weight_loading
 
 
 def Writing_of_the_PDstrip_input_file(masses_filename, coordinates_filename, Lpp):
@@ -112,32 +116,37 @@ def Writing_of_the_PDstrip_input_file(masses_filename, coordinates_filename, Lpp
         it is the length between perpendiculars
     :returns
     nothing, it writes a file with the data needed """
-    all_coord = collection_of_coordinates(coordinates_filename)  # list of the coordinates
-    all_coord = all_coord.conversion_coordinate_to_pdstrip(Lpp / 2)
-    all_coord = all_coord.correction_of_coordinates()
-    all_coord.checking()
-    all_coord.plot_one_frame(0)
-    all_coord.plotting()
-    weightloading = collection_of_mass(masses_filename)  # list of the masses
-    weightloading.pdstrip_coordinates(Lpp / 2)
-    weightloading.plot_loading(-Lpp / 2, Lpp / 2)
-    f = open("data_pdstrip.csv", "w")  # writing of the info in the file "data_pdstrip.csv"
+    hull_form = collection_of_coordinates(coordinates_filename)  # list of the coordinates
+    hull_form = hull_form.conversion_coordinate_to_pdstrip(Lpp / 2)
+    hull_form = hull_form.correction_of_coordinates()
+    hull_form.checking()
+    hull_form.plot_one_frame(0)
+    hull_form.plotting()
+    weight_loading = collection_of_mass(masses_filename)  # list of the masses
+    weight_loading.pdstrip_coordinates(Lpp / 2)
+    weight_loading.plot_loading(-Lpp / 2, Lpp / 2)
+    file = open("data_pdstrip.csv", "w")  # writing of the info in the file "data_pdstrip.csv"
     # for every section we have the backward and the forward
-    for i in range(len(all_coord.shape) - 1):
-        back = (all_coord.shape[i].x + all_coord.shape[i + 1].x) / 2
-        forw = conversion_for_pdstrip_xaxis(Lpp, Lpp / 2)
-        m = weightloading.mass_calculation(back, forw)
+    for i in range(len(hull_form.shape) - 1):
+        back_section = (hull_form.shape[i].x + hull_form.shape[i + 1].x) / 2
+        front_section = conversion_for_pdstrip_xaxis(Lpp, Lpp / 2)
+        weight_o_the_current_part = weight_loading.mass_calculation(back_section, front_section)
         try:
-            xg, yg, zg = weightloading.calcul_center_of_gravity(back, forw)
+            x_coordinate_CoG, y_coordinate_CoG, z_coordinate_CoG = \
+                weight_loading.calcul_center_of_gravity(back_section, front_section)
         except ZeroDivisionError:
-            xg, yg, zg = all_coord.center_of_gravity_no_mass(back, forw)
-        rx2, ry2, rz2, xy, yz, xz = all_coord.calcul_all(back, forw, xg, yg, zg)
-        data = [m, xg, yg, zg, rx2, ry2, rz2, xy, yz, xz]
-        for inf in data:
+            x_coordinate_CoG, y_coordinate_CoG, z_coordinate_CoG = \
+                hull_form.center_of_gravity_no_mass(back_section, front_section)
+        radius_of_inertia_x_square, radius_of_inertia_y_square, radius_of_inertia_z_square, xy, yz, xz = \
+            hull_form.calcul_all(back_section, front_section, x_coordinate_CoG, y_coordinate_CoG, z_coordinate_CoG)
+        data = [weight_o_the_current_part, x_coordinate_CoG, y_coordinate_CoG, \
+                z_coordinate_CoG, radius_of_inertia_x_square, radius_of_inertia_y_square, radius_of_inertia_z_square,\
+                xy, yz, xz]
+        for input_value in data:
             # we write every input for the section
-            f.write(str(inf) + " ")
-        f.write("\n")
-    f.close()  # total mass is checked
+            file.write(str(input_value) + " ")
+        file.write("\n")
+    file.close()  # total mass is checked
     return
 
 
