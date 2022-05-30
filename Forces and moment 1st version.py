2b69a5eb8df99289fc2960ffbcb695773e070cdc
jules <jules.dimauro@gmail.com>
2022-05-30 13:07:17 +0200

    nouvelle version

--- Forces and moment 1st version.py
+++ Forces and moment 1st version.py
@@ -28,15 +28,15 @@ def graph_file_for_one_wave(filename, wave_frequency, wave_length, wave_angle, w
     It plots 6 graphs for the wave selected and for the part selected (real, imaginary or absolute)
     """
     if text== "real":
-        const=0
-    elif text== "im":
-        const=1
-    elif text== "abs":
-        const=2
+        constant=0
+    elif text== "imaginary":
+        constant=1
+    elif text== "absolute":
+        constant=2
     file = open(filename, "r", encoding="utf-8")
     the_lines = csv.reader(file)
     line_counter = 0
-    les_x = []
+    list_x_coordinates = []
     forces_along_x = []
     forces_along_y = []
     forces_along_z = []
@@ -44,63 +44,64 @@ def graph_file_for_one_wave(filename, wave_frequency, wave_length, wave_angle, w
     moment_along_y = []
     moment_along_z = []
     number_of_section = 0
-    ex_wave_frequency = 0
-    ex_wave_length = 0
-    ex_wave_angle = 0
-    ex_speed = 0
+    example_wave_frequency = 0
+    example_wave_length = 0
+    example_wave_angle = 0
+    example_wave_speed = 0
     for line in the_lines:
         try:
-            new = line[0].strip().split()
+            line_formatted = line[0].strip().split()
         except IndexError:
             pass
-        print(new)
-        if len(new) == 0:
-            new = ["error", "error", "error"]
-        if new[0] == "wave" and new[2] == "frequency":
-            ex_wave_frequency = float(new[3])
-        if new[0] == "wave" and new[1] == "length":
+        print(line_formatted)
+        if len(line_formatted) == 0:
+            line_formatted = ["error", "error", "error"]
+        if line_formatted[0] == "wave" and line_formatted[2] == "frequency":
+            example_wave_frequency = float(line_formatted[3])
+        if line_formatted[0] == "wave" and line_formatted[1] == "length":
             try:
-                ex_wave_length = float(new[2])
+                example_wave_length = float(line_formatted[2])
             except:
-                ex_wave_length = 100000
-        if new[0] == "wave" and new[1] == "angle":
-            ex_wave_angle = float(new[2])
-        if new[0] == "speed":
-            ex_speed = float(new[1])
-        if new[0] == "Number":
-            number_of_section = float(new[3])
-        if ex_wave_length == wave_length and ex_wave_angle == wave_angle and ex_wave_frequency == wave_frequency and ex_speed == wave_speed:
-            if new[0] == "Force":
-                x = float(new[1])
-                el_force_x = float(new[2+const])
-                el_force_y = float(new[5+const])
-                el_force_z = float(new[8+const])
-                forces_along_x.append(el_force_x)
-                forces_along_y.append(el_force_y)
-                forces_along_z.append(el_force_z)
-                if x not in les_x:
-                    les_x.append(x)
+                example_wave_length = 100000
+        if line_formatted[0] == "wave" and line_formatted[1] == "angle":
+            example_wave_angle = float(line_formatted[2])
+        if line_formatted[0] == "speed":
+            example_wave_speed = float(line_formatted[1])
+        if line_formatted[0] == "Number":
+            number_of_section = float(line_formatted[3])
+        if example_wave_length == wave_length and example_wave_angle == wave_angle and \
+                example_wave_frequency == wave_frequency and example_wave_speed == wave_speed:
+            if line_formatted[0] == "Force":
+                x_coordinate = float(line_formatted[1])
+                element_force_x = float(line_formatted[2+constant])
+                element_force_y = float(line_formatted[5+constant])
+                element_force_z = float(line_formatted[8+constant])
+                forces_along_x.append(element_force_x)
+                forces_along_y.append(element_force_y)
+                forces_along_z.append(element_force_z)
+                if x_coordinate not in list_x_coordinates:
+                    list_x_coordinates.append(x_coordinate)
             if line_counter == (number_of_section - 1):
                 break
-            if new[0] == "Moment":
+            if line_formatted[0] == "Moment":
                 line_counter += 1
-                el_moment_x = float(new[1+const])
-                el_moment_y = float(new[4+const])
-                el_moment_z = float(new[7+const])
-                moment_along_x.append(el_moment_x)
-                moment_along_y.append(el_moment_y)
-                moment_along_z.append(el_moment_z)
+                element_moment_x = float(line_formatted[1+constant])
+                element_moment_y = float(line_formatted[4+constant])
+                element_moment_z = float(line_formatted[7+constant])
+                moment_along_x.append(element_moment_x)
+                moment_along_y.append(element_moment_y)
+                moment_along_z.append(element_moment_z)
     print(forces_along_x)
     print(forces_along_y)
     print(forces_along_z)
     print(moment_along_x)
     print(moment_along_y)
     print(moment_along_z)
-    all_el = [forces_along_x, forces_along_y, forces_along_z, moment_along_x, moment_along_y, moment_along_z]
+    all_graph = [forces_along_x, forces_along_y, forces_along_z, moment_along_x, moment_along_y, moment_along_z]
     list_title = ["x", "y", "z"]
-    n_all_el = len(all_el)
-    for i in range(n_all_el):
-        plt.plot(les_x, all_el[i])
+    n_all_graph = len(all_graph)
+    for i in range(n_all_graph):
+        plt.plot(list_x_coordinates, all_graph[i])
         if i <= 2:
             plt.title("Forces along " + list_title[i] + " axis" + " " + text)
         if i > 2:
@@ -109,4 +110,4 @@ def graph_file_for_one_wave(filename, wave_frequency, wave_length, wave_angle, w
     return
 
 
-graph_file_for_one_wave("pdstrip.out.ok", 0.2, 1540.43, 0, 0,"abs")
+graph_file_for_one_wave("pdstrip.out.ok", 0.2, 1540.43, 0, 0,"absolute")
