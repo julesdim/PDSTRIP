da9a22c5dbc3f56414536c56b22f297bb5879d4c
julesdim <101173772+julesdim@users.noreply.github.com>
2022-06-10 08:27:02 +0200

    first

--- calculation_of_frame_parameters_v2.py
+++ calculation_of_frame_parameters_v2.py
@@ -112,6 +112,7 @@ def collection_of_mass(filename: str):
                 current_mass.calcul_xg_not_the_mid()
                 print(current_mass.linear_density_start, current_mass.linear_density_end)
             weight_loading.__append__(current_mass)
+    print(total_mass,"TOTAL")
     return weight_loading
 
 
